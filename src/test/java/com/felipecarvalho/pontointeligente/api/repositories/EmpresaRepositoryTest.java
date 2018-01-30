352c77c081c7d2b5c6d5dd01d00e63da3b36b801
Felipe <ramos.felippe@gmail.com>
2018-01-30 19:55:39 -0200

    -Criação das classes de entidades -Criação do flyway (migrations de banco de dados) -Criação das classes de testes

---
+++ src/test/java/com/felipecarvalho/pontointeligente/api/repositories/EmpresaRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.felipecarvalho.pontointeligente.api.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.felipecarvalho.pontointeligente.api.entities.Empresa;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class EmpresaRepositoryTest {
+	
+	//Para rodar o teste, clica com o botão direito sobre o nome da classe e clica em
+	//Run As -> JUnit test
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private static final String CNPJ = "51463645000100";
+
+	@Before //JUnit: método que executa antes de rodarem os testes
+	public void setUp() throws Exception {
+		Empresa empresa = new Empresa();
+		empresa.setRazaoSocial("Empresa de exemplo");
+		empresa.setCnpj(CNPJ);
+		this.empresaRepository.save(empresa);
+	}
+	
+	@After //JUnit: método que executa depois de rodarem os testes
+    public final void tearDown() { 
+		this.empresaRepository.deleteAll();
+	}
+
+	@Test
+	public void testBuscarPorCnpj() {
+		Empresa empresa = this.empresaRepository.findByCnpj(CNPJ);
+		
+		assertEquals(CNPJ, empresa.getCnpj());
+	}
+
+}
